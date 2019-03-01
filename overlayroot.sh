f1be71102e04c420f96083a751b2853f35144d9c
takumin <takumiiinn@gmail.com>
2019-03-02 00:34:28 +0900

    fix

--- overlayroot.sh
+++ overlayroot.sh
@@ -490,7 +490,7 @@ fi
 
 # Mount Root File System Partition
 mkdir -p "${WORKDIR}"
-mount -t tmpfs -o 'size=90%,mode=0755' tmpfs "${WORKDIR}"
+mount -t tmpfs -o 'size=4g,mode=0755' tmpfs "${WORKDIR}"
 
 ################################################################################
 # Debootstrap
