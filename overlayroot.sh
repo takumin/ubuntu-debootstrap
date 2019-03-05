d7992b0a5dbcda8da09a38cc6609d8790dfa3be0
takumin <takumiiinn@gmail.com>
2019-03-05 13:44:40 +0900

    fix

--- overlayroot.sh
+++ overlayroot.sh
@@ -958,58 +958,58 @@ if [[ "${PROFILE}" =~ ^.*cloud.*$ ]]; then
 	# Initialize Cloud-Init
 	cat > "${WORKDIR}/etc/cloud.cfg" <<- '__EOF__'
 	cloud_init_modules:
-	 - migrator
-	 - seed_random
-	 - bootcmd
-	 - write-files
-	 - growpart
-	 - resizefs
-	 - disk_setup
-	 - mounts
-	 - set_hostname
-	 - update_hostname
-	 - update_etc_hosts
-	 - ca-certs
-	 - rsyslog
-	 - users-groups
-	 - ssh
+	- migrator
+	- seed_random
+	- bootcmd
+	- write-files
+	- growpart
+	- resizefs
+	- disk_setup
+	- mounts
+	- set_hostname
+	- update_hostname
+	- update_etc_hosts
+	- ca-certs
+	- rsyslog
+	- users-groups
+	- ssh
 
 	cloud_config_modules:
-	 - emit_upstart
-	 - snap
-	 - ssh-import-id
-	 - locale
-	 - set-passwords
-	 - grub-dpkg
-	 - apt-pipelining
-	 - apt-configure
-	 - ubuntu-advantage
-	 - ntp
-	 - timezone
-	 - disable-ec2-metadata
-	 - runcmd
-	 - byobu
+	- emit_upstart
+	- snap
+	- ssh-import-id
+	- locale
+	- set-passwords
+	- grub-dpkg
+	- apt-pipelining
+	- apt-configure
+	- ubuntu-advantage
+	- ntp
+	- timezone
+	- disable-ec2-metadata
+	- runcmd
+	- byobu
 
 	cloud_final_modules:
-	 - package-update-upgrade-install
-	 - fan
-	 - landscape
-	 - lxd
-	 - puppet
-	 - chef
-	 - mcollective
-	 - salt-minion
-	 - rightscale_userdata
-	 - scripts-vendor
-	 - scripts-per-once
-	 - scripts-per-boot
-	 - scripts-per-instance
-	 - scripts-user
-	 - ssh-authkey-fingerprints
-	 - keys-to-console
-	 - phone-home
-	 - final-message
-	 - power-state-change
+	- package-update-upgrade-install
+	- fan
+	- landscape
+	- lxd
+	- puppet
+	- chef
+	- mcollective
+	- salt-minion
+	- rightscale_userdata
+	- scripts-vendor
+	- scripts-per-once
+	- scripts-per-boot
+	- scripts-per-instance
+	- scripts-user
+	- ssh-authkey-fingerprints
+	- keys-to-console
+	- phone-home
+	- final-message
+	- power-state-change
 	__EOF__
 
 	# Generate Network Config from MetaData Server
